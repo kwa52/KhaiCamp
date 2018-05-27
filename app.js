b27ef0304ad5ad81fc3d68eb89396a340ba99499
kwa52 <kwa52@sfu.ca>
2018-05-27 11:50:49 -0700

    Change listening port

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
+app.listen(process.env.PORT, process.env.IP);
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
